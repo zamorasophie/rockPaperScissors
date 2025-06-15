d876fcc6d0f23fa514c5ee0340ff7434d2730444
Sophie Zamora <163759166+zamorasophie@users.noreply.github.com>
2025-06-15 16:35:26 +0200

    add computerchoice function

--- javascript.js
+++ javascript.js
@@ -1,2 +1,14 @@
+function getComputerChoice() {
 
-console.log("Hello, world!")
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return "rock"
+        case 1:
+            return "paper"
+        case 2:
+            return "scissors"
+    }
+}
+
+console.log(getComputerChoice())
